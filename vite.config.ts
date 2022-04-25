2c1cfa6b0a8c9191219553f41f0ad6f32524b701
hongjinwu <hongjinwu@trustnote.cn>
2022-04-25 17:13:48 +0800

    chore: 升级vite-plugin-eslint 依赖版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    eslintPlugin({
+    eslintPlugin(),
+    vueJsx({
       // 配置选项
-      cache: false // 禁用 eslint 缓存
     })
   ]
 })
