8e1dc81757c70ae0e191ac58fca0db228eeaa33a
hongjinwu <hongjinwu@trustnote.cn>
2022-04-26 15:52:32 +0800

    chore: 封装泛型请求方法

--- src/api/common.ts
+++ src/api/common.ts
@@ -3,13 +3,23 @@
  */
 import request from '@/utils/request'
 
-interface ResponseData<T = any> {
-  status: number
-  msg: string
-  data: T
-}
+// interface ResponseData<T = any> {
+//   status: number
+//   msg: string
+//   data: T
+// }
 
 export const getLoginInfo = () => {
+  return request<{
+    logo_square: string
+    logo_rectangle: string
+    login_logo: string
+    slide: string[]
+  }>({
+    method: 'GET',
+    url: '/login/info'
+  })
+
   // return request.get<{
   //   status: number
   //   msg: string
@@ -20,10 +30,11 @@ export const getLoginInfo = () => {
   //     slide: string[]
   //   }
   // }>('/login/info')
-  return request.get<ResponseData<{
-    logo_square: string
-    logo_rectangle: string
-    login_logo: string
-    slide: string[]
-  }>>('/login/info')
+
+  // return request.get<ResponseData<{
+  //   logo_square: string
+  //   logo_rectangle: string
+  //   login_logo: string
+  //   slide: string[]
+  // }>>('/login/info')
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 const request = axios.create({
   baseURL: 'https://shop.fed.lagou.com/api/admin'
@@ -15,9 +15,13 @@ request.interceptors.request.use(config => {
 // 响应拦截器
 request.interceptors.response.use(response => {
   // 统一处理接口响应错误, 比如 token 过期无效、服务的异常等
-  return response.data
+  return response
 }, error => {
   return Promise.reject(error)
 })
 
-export default request
+export default <T = any>(config: AxiosRequestConfig) => {
+  return request(config).then(res => {
+    return res.data.data as T
+  })
+}
