79394b6cc1aa06e5ef96a60e1a0f4e87bc951da6
hongjinwu <hongjinwu@trustnote.cn>
2022-04-26 12:05:49 +0800

    chore: vite配置路径别名

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,11 +3,15 @@
     首页
     {{ $store.state.count }}
     {{ store.state.count }}
+    <HelloWorld />
   </div>
 </template>
 
 <script setup lang="ts">
 import { useStore } from '../../store'
+import User from '@/api/user'
+import HelloWorld from '@comp/HelloWorld.vue'
+console.log(User)
 
 const store = useStore()
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,11 @@ export default defineConfig({
     vueJsx({
       // 配置选项
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+      '@comp': path.join(__dirname, 'src/components')
+    }
+  }
 })
